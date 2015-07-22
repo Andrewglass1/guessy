82faabf1cc168ee087be34fa77fc70d64e7f90d6
Andrew Glass <andrewglass1@gmail.com>
2015-07-21 20:54:30 -0400

    rounds controller works with angular

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,4 +1,5 @@
 class RoundsController < ApplicationController
+  skip_before_action :verify_authenticity_token
   before_action :load_room, only: [:create]
 
   def edit
@@ -7,12 +8,23 @@ class RoundsController < ApplicationController
 
   def create
     round = @room.attach_round(params.slice("topic_id", "suggested_prompt_id", "prompt"))
-    if round.answers.any?
-      round.activate!
-      redirect_to room_path(@room)
-    else
-      redirect_to edit_round_path(round)
+    round.activate! if round.answers.any?
+    respond_to do |format|
+      format.json do
+        render json: {room: @room}
+      end
+
+      format.html do
+        if round.answers.any?
+          round.activate!
+          redirect_to room_path(@room)
+        else
+          redirect_to edit_round_path(round)
+        end
+      end
     end
+
+
   end
 
   def update
